2fa027742a7b827e5fafc84e718c9bc9d53f01d9
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-08-17 20:24:25 +0900

    Fix 소켓 중복 연결 수정

--- kanban-board/client/src/App.js
+++ kanban-board/client/src/App.js
@@ -7,8 +7,9 @@ import List from "./components/List/List";
 import * as S from "./Style";
 import User from "./components/User/User";
 
+export const socket = io("http://localhost:4000");
+
 const App = () => {
-  const socket = io("http://localhost:4000");
   const [userList, setUserList] = useState([]);
   //고유 아이디(rgb값 랜덤으로 생성)
   const num = useRef([
@@ -42,64 +43,61 @@ const App = () => {
   const [columns, setColumns] = useState(columsList);
 
   //결과 재정렬 함수
-  const reorder = useCallback(
-    (result, columns, setColumns) => {
-      //범위 밖으로 떨구면 아무것도 안함
-      if (!result.destination) return;
+  const reorder = useCallback((result, columns, setColumns) => {
+    //범위 밖으로 떨구면 아무것도 안함
+    if (!result.destination) return;
 
-      // 다른 컬럼으로 이동시
-      if (result.source.droppableId !== result.destination.droppableId) {
-        //출발, 도착 컬럼 정보
-        const sourceColumn = columns[result.source.droppableId];
-        const destColumn = columns[result.destination.droppableId];
+    // 다른 컬럼으로 이동시
+    if (result.source.droppableId !== result.destination.droppableId) {
+      //출발, 도착 컬럼 정보
+      const sourceColumn = columns[result.source.droppableId];
+      const destColumn = columns[result.destination.droppableId];
 
-        //촐발, 도착 컬럼의 아이템
-        const sourceItems = [...sourceColumn.items];
-        const destItems = [...destColumn.items];
+      //촐발, 도착 컬럼의 아이템
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
 
-        //드래그한 item
-        const [remove] = sourceItems.splice(result.source.index, 1);
-        //도착 컬럼 아이템에 삽입
-        destItems.splice(result.destination.index, 0, remove);
-        socket.emit(
-          "toOntherColumn",
-          result.source.droppableId,
-          sourceItems,
-          result.destination.droppableId,
-          destItems
-        );
-        setColumns({
-          ...columns,
-          [result.source.droppableId]: {
-            ...sourceColumn,
-            items: sourceItems,
-          },
-          [result.destination.droppableId]: {
-            ...destColumn,
-            items: destItems,
-          },
-        });
-      }
+      //드래그한 item
+      const [remove] = sourceItems.splice(result.source.index, 1);
+      //도착 컬럼 아이템에 삽입
+      destItems.splice(result.destination.index, 0, remove);
+      socket.emit(
+        "toOntherColumn",
+        result.source.droppableId,
+        sourceItems,
+        result.destination.droppableId,
+        destItems
+      );
+      setColumns({
+        ...columns,
+        [result.source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [result.destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    }
 
-      // 같은 컬럼에서 순서만 바꿀 때
-      else {
-        const column = columns[result.source.droppableId];
-        const copiedItems = [...column.items];
+    // 같은 컬럼에서 순서만 바꿀 때
+    else {
+      const column = columns[result.source.droppableId];
+      const copiedItems = [...column.items];
 
-        const [remove] = copiedItems.splice(result.source.index, 1);
-        copiedItems.splice(result.destination.index, 0, remove);
-        socket.emit("defaultEvent", result.source.droppableId, copiedItems);
-        setColumns({
-          ...columns,
-          [result.source.droppableId]: {
-            ...column,
-            items: copiedItems,
-          },
-        });
-      }
-    },
-    [socket]
-  );
+      const [remove] = copiedItems.splice(result.source.index, 1);
+      copiedItems.splice(result.destination.index, 0, remove);
+      socket.emit("defaultEvent", result.source.droppableId, copiedItems);
+      setColumns({
+        ...columns,
+        [result.source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
+  }, []);
 
   //아이템 생성 함수
   const addItem = useCallback(
@@ -119,7 +117,7 @@ const App = () => {
         },
       });
     },
-    [socket, columns]
+    [columns]
   );
 
   //아이템 삭제 함수
@@ -137,7 +135,7 @@ const App = () => {
         },
       });
     },
-    [socket, columns]
+    [columns]
   );
 
   //최초 접속시
@@ -160,7 +158,7 @@ const App = () => {
         )
       );
     });
-  }, [socket]);
+  });
   //아이템 변화 발생시
   useEffect(() => {
     socket.on(
@@ -188,7 +186,7 @@ const App = () => {
         },
       });
     });
-  }, [columns, socket]);
+  }, [columns]);
 
   return (
     <>
