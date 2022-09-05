e0445c7098af7755e9acfcab06e4cd1cf7f296c5
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-09-05 22:43:39 +0900

    Refactor 접속 종료 useEffect 병합

--- kanban-board/client/src/components/User/User.js
+++ kanban-board/client/src/components/User/User.js
@@ -8,28 +8,26 @@ const User = () => {
   const [userList, setUserList] = useState([]);
   //고유 아이디(rgb값 랜덤으로 생성)
   const num = useRef(userId);
-  //최초 접속시
+  /** 접속중인 유저 리스트 변경 */
+  const editUserList = (data) => {
+    setUserList(
+      Array.from(data).filter(
+        (x) => JSON.stringify(x) !== JSON.stringify(num.current)
+      )
+    );
+  };
+
+  //접속 혹은 종료한 유저 발생시 자신을 제외한 접속한 유저 렌더링
   useEffect(() => {
     socket.emit("join", num.current);
     socket.on("join", (data) => {
-      //자신을 제외한 접속한 유저 렌더링
-      setUserList(
-        Array.from(data).filter(
-          (x) => JSON.stringify(x) !== JSON.stringify(num.current)
-        )
-      );
+      editUserList(data);
+    });
+    socket.on("left", (data) => {
+      editUserList(data);
     });
   }, [socket]);
-  //접속 종료시
-  useEffect(() => {
-    socket.on("left", (data) => {
-      setUserList(
-        Array.from(data).filter(
-          (x) => JSON.stringify(x) !== JSON.stringify(num.current)
-        )
-      );
-    });
-  });
+
   return (
     <S.Container>
       {userList.map((item, index) => (
