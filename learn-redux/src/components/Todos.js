9cddf42c661a539dd604f2103b153d9441cd03b1
leesb971204 <leesb971204@gmail.com>
2022-09-26 09:40:53 +0900

    test

---
+++ learn-redux/src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
+const Todos = ({ todos, onCreate, onToggle }) => {
+  const [text, setText] = useState("");
+  const onChange = (e) => setText(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onCreate(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={text} placeholder="input" onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+};
+
+export default Todos;
